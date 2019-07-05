acb5434fa07e4f8705bce0f9440c596aaf2b0de5
seleuchel <37205198+seleuchel@users.noreply.github.com>
2019-07-05 19:47:40 +0900

    Update redmine_DB_backup.sh

--- redmine_DB_backup.sh
+++ redmine_DB_backup.sh
@@ -2,7 +2,7 @@
 cd /home/bitnami/backto
 Date=$(date +"%m-%d-%Y-T-%H-%M-%S")
 echo $Date
-./mysqldump -ubitnami -peb76b3fdd5 bitnami_redmine > /home/bitnami/backupbox/redmine_backup_$Date.sql
+./mysqldump -ubitnami -p[[DBpassword]] bitnami_redmine > /home/bitnami/backupbox/redmine_backup_$Date.sql
 cd /home/bitnami/backupbox
 #tar -cvzf redmine_backup_$Date.tar.gz redmine_backup_$Date.sql
 chmod 755 redmine_backup_$Date.sql
